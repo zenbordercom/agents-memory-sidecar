76b832e983d37af5bdf990ca35ea901a87cae08e
Zephyr <zenbordercom@users.noreply.github.com>
2026-07-02 21:39:27 +0000

    Make observation prune grant optional

--- migrations/002_observation_prune_grant.sql
+++ migrations/002_observation_prune_grant.sql
@@ -1,1 +1,7 @@
-GRANT DELETE ON agent_observations TO agent_memory_app;
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'agent_memory_app') THEN
+    GRANT DELETE ON agent_observations TO agent_memory_app;
+  END IF;
+END
+$$;
